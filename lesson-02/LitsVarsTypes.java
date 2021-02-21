d7086b6719868baf197877bbfe412de573d23ccd
Blade Frisch <blade.frisch@gmail.com>
2021-02-21 12:57:32 -0500

    Added code for lesson 2

---
+++ lesson-02/LitsVarsTypes.java
@@ -0,0 +1,20 @@
+public class LitsVarsTypes {
+
+    public static void main(String[] args) {
+        int myAge = 875;
+        double gpa = 3.75;
+        char lastInitial = 'F';
+        boolean isTeacher = true;
+
+        int separated; // Declaration
+        separated = 5; // Initialization
+
+        separated = 10; // Reassignment
+
+        System.out.println(myAge);
+        System.out.println(gpa);
+        System.out.println(lastInitial);
+        System.out.println(isTeacher);
+    }
+
+}
